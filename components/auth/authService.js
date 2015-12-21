eda6721432a14996602385d49a38a9db5199eaa4
jiteshjha <jiteshjha96@gmail.com>
2015-12-22 04:55:12 +0530

    Added User Authentication

---
+++ components/auth/authService.js
@@ -0,0 +1,13 @@
+// components/auth/authService.js
+
+(function()
+	'use strict';
+
+	angular
+		.module('statusApp')
+		.factory('Auth', AuthService);
+	function AuthService($firebaseAuth) {
+		var ref = new Firebase("https://statusapp.firebaseio.com");
+		return $firebaseAuth(ref);
+	}
+})();
